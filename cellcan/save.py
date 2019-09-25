80182a5f0f67bfaf375514c1c217f91329d88ee7
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2019-09-25 12:01:14 -0400

    Create save.py

---
+++ cellcan/save.py
@@ -0,0 +1,7 @@
+
+import time
+def save(data):
+     # we need to save the time that this sample is taken
+     time[i] = current time and number of samples
+     # this needs to be static so that we don't just send the last sample
+     # also we need to have a send limit, otherwise we will eventually run out of data
