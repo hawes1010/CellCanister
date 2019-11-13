639066ff134aa5f0da74b694b4b75cebda9a756d
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2019-11-13 14:44:47 -0500

    Create Spod.h

---
+++ S_POD/SPod_Functions/Spod.h
@@ -0,0 +1,21 @@
+//Spod.h
+#ifndef Spod_h
+#define Spod_h
+
+#include "Arduino.h"
+
+class Spod 
+{
+  public:
+    Spod();
+    void getLogger();
+	void sonic();
+	void read_C();
+	void read_A();
+	void read_D();
+	void read_S();
+	void read_R();
+	void read_?();
+};
+
+#endif
